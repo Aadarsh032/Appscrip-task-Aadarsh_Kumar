d7351e2078ce67b44ce2f044b71cadc015e466f8
Aadarsh032 <kumarn206@gmail.com>
2025-04-24 22:34:28 +0530

    Footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,38 +4,38 @@ import Image from 'next/image'
 
 const Footer = () => {
     return (
-        <div className='footerMain' >
-            <div className='footerTop' >
-                <div className='footerTopLeft'>
-                    <h4 className='footerTitles' >BE THE FIRST TO KNOW</h4>
-                    <p className='footerListItems'>Sign up for updates from mettā muse</p>
+        <div className='footer-main' >
+            <div className='footer-top' >
+                <div className='footer-top-left'>
+                    <h4 className='footer-titles' >BE THE FIRST TO KNOW</h4>
+                    <p className='footer-list-items'>Sign up for updates from mettā muse</p>
                     <div>
-                        <input type='text' id='emailInput' placeholder='Enter Your Email Id ...' />
-                        <button id='subscribeButton' >SUBSCRIBE</button>
+                        <input type='text' id='email-input' placeholder='Enter Your Email Id ...' />
+                        <button id='subscribe-button' >SUBSCRIBE</button>
                     </div>
                 </div>
-                <div className='footerTopRight'>
+                <div className='footer-top-right'>
                     <div>
-                        <h4 className='footerTitles'>CONTACT US</h4>
-                        <p className='footerListItems' style={{ fontSize: '14px' }}>+44 221 133 5360</p>
-                        <p className='footerListItems' style={{ fontSize: '14px' }}>customercare@mettamuse.com</p>
+                        <h4 className='footer-titles'>CONTACT US</h4>
+                        <p className='footer-list-items' style={{ fontSize: '14px' }}>+44 221 133 5360</p>
+                        <p className='footer-list-items' style={{ fontSize: '14px' }}>customercare@mettamuse.com</p>
                     </div>
                     <div>
                         <h4>CURRENCY</h4>
                         <div>
-                            <Image src={'/assets/USA.png'} alt={'currency_icon'} width={24} height={24} style={{ borderRadius: '100%' }} />
+                            <Image src={'/assets/USA.png'} alt={'Currency Icon'} width={24} height={24} style={{ borderRadius: '100%' }} />
                             <span>USD</span>
                         </div>
-                        <p className='footerListItems' style={{ fontSize: '12px' }}>Transactions will be completed in Euros and a currency reference is available on hover.</p>
+                        <p className='footer-list-items' style={{ fontSize: '12px' }}>Transactions will be completed in Euros and a currency reference is available on hover.</p>
                     </div>
 
                 </div>
             </div>
             <hr />
-            <div className='footerMiddle'>
-                <div className='footerMiddle_Left'>
-                    <h4 className='footerTitles'>mettā muse</h4>
-                    <ul className='footerListItems'>
+            <div className='footer-middle'>
+                <div className='footer-middle-left'>
+                    <h4 className='footer-titles'>mettā muse</h4>
+                    <ul className='footer-list-items'>
                         <li>About Us</li>
                         <li>Stories</li>
                         <li>Artisans</li>
@@ -44,9 +44,9 @@ const Footer = () => {
                         <li>EU Compliances Docs</li>
                     </ul>
                 </div>
-                <div className='footerMiddle_Middle'>
-                    <h4 className='footerTitles'>QUICK LINKS</h4>
-                    <ul className='footerListItems'>
+                <div className='footer-middle-middle'>
+                    <h4 className='footer-titles'>QUICK LINKS</h4>
+                    <ul className='footer-list-items'>
                         <li>Orders & Shipping</li>
                         <li>Join/Login as a Seller</li>
                         <li>Payment & Pricing</li>
@@ -56,14 +56,14 @@ const Footer = () => {
                         <li>Terms & Conditions</li>
                     </ul>
                 </div>
-                <div className='footerMiddle_Right'>
-                    <h4 className='footerTitles'>FOLLOW US</h4>
+                <div className='footer-middle-right'>
+                    <h4 className='footer-titles'>FOLLOW US</h4>
                     <div className='social-icons'>
                         <Image src={'/assets/insta.svg'} alt={'Instagram'} width={30} height={30} />
                         <Image src={'/assets/linkedin.svg'} alt={'Linkedin'} width={30} height={30} />
 
                     </div>
-                    <h4 className='footerTitles'>mettā muse Accepts</h4>
+                    <h4 className='footer-titles-down-right'>mettā muse Accepts</h4>
                     <div className='payment-icons'>
                         <Image src={'/assets/gpay.svg'} alt={'Google Pay'} width={50} height={50} />
                         <Image src={'/assets/mastercard.svg'} alt={'MasterCard'} width={50} height={50} />
